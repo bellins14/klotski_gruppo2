345c321f14ca83d698da8cd57b3427e27221820d
bellins14_PowerShell_Win11 <tommaso.bellinato@gmail.com>
2023-04-27 11:47:05 +0200

    renamed Main, Controller, view

--- src/main/java/com/project/klotski/Controller.java
+++ src/main/java/com/project/klotski/Controller.java
@@ -3,7 +3,7 @@ package com.project.klotski;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
