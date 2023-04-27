667151b266abd1b926c42c1e2af90d62f74973cd
bellins14_PowerShell_Win11 <tommaso.bellinato@gmail.com>
2023-04-27 12:08:00 +0200

    new file:   src/main/resources/com/project/klotski/style.css

--- src/main/java/com/project/klotski/Main.java
+++ src/main/java/com/project/klotski/Main.java
@@ -6,12 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        String css = Objects.requireNonNull(Main.class.getResource("style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
