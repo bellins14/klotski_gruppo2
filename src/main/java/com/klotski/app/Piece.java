26a481a4dda2137e2ade1fa960b04b996a179b6e
damianocaon <damiano.caon@studenti.unipd.it>
2023-06-03 15:06:34 +0200

    Update Piece
    Aggiornata la classe piece con javadoc e metodi per jackson. Tutto funziona.

--- src/main/java/com/klotski/app/Piece.java
+++ src/main/java/com/klotski/app/Piece.java
@@ -7,66 +7,111 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.Objects;
 
+/**
+ * Classe che rappresenta un pezzo del Klotski.
+ */
 public class Piece extends Rectangle {
 
-    //
+    //colore del Piece
+    //private final Color _color;
     private String _image = "img/";
 
-    //==============
-    // CONSTRUCTORS
-    //==============
-    //default
+
+    /**
+     * Costruttore di default, che inizializza un blocco 100x100.
+     */
     public Piece(){
         //richiamo il costruttore del rettangolo
         super();
-        //decido che il piece di default è un quadrato (100x100)
+
         this.setType(0);
-        //immagine predefinta per il quadrato
         this.setImageFill("piece0.png");
         //assegno un bordo nero di spessore 3
         this.setStroke(Color.BLACK);
         this.setStrokeWidth(3);
     }
-    //con parametri
+
+
+    /*  Ricordarsi che 100x100 è 100 di larghezza per 100 di altezza, mentre le coordinate del JSON sono
+        invertite.
+     */
+    /**
+     * Costruttore che inizializza un blocco a partire dal tipo e dalla sua skin.
+     * @param pieceType tipo del pezzo: 0 = 100x100; 1 = 100x200; 2 = 200x100; 3 = 300x300.
+     * @param imageName nome dell'immagine che fa da skin al blocco.
+     */
     public Piece(int pieceType, String imageName) {
         //richiamo il costruttore del rettangolo
         super();
-        //in base al numero del pieceType va a creare il piece scelto
+
         this.setType(pieceType);
-        //immagine predefinita per il piece scelto
         this.setImageFill(imageName);
         //assegno un bordo nero di spessore 3
         this.setStroke(Color.BLACK);
         this.setStrokeWidth(3);
     }
 
+
+    /**
+     * Costruttore con altezza e larghezza.
+     * @param h altezza del blocco.
+     * @param w larghezza del blocco.
+     */
+    public Piece(int h, int w){
+        super();
+        if (h == 100 && w == 100) {
+            this.setType(0);
+            this.setImageFill("piece0.png");
+        } else if (h == 200 && w == 100) {
+            this.setType(1);
+            this.setImageFill("piece1.png");
+        } else if (h == 100 && w == 200) {
+            this.setType(2);
+            this.setImageFill("piece2.png");
+        } else if (h == 200 && w == 200) {
+            this.setType(3);
+            this.setImageFill("piece3.png");
+        }
+        this.setStroke(Color.BLACK);
+        this.setStrokeWidth(3);
+    }
+
+
+    /**
+     * Metodo che setta le dimensioni del blocco (e l'id) in base al suo tipo.
+     * @param pieceType tipo del blocco.
+     */
     public void setType(int pieceType){
         //in base all'argomento passato capisco quale blocco creare
         switch (pieceType) {
             case 0 -> {
                 this.setHeight(100);
                 this.setWidth(100);
-                //this.setId("a");
+                this.setId("a");
             }
             case 1 -> {
                 this.setHeight(200);
                 this.setWidth(100);
-                //this.setId("b");
+                this.setId("b");
             }
             case 2 -> {
                 this.setHeight(100);
                 this.setWidth(200);
-                //this.setId("c");
+                this.setId("c");
             }
             case 3 -> {
                 this.setHeight(200);
                 this.setWidth(200);
-               // this.setId("d");
+                this.setId("d");
             }
         }
     }
 
-    // controllo eccezione?
+
+    /**
+     * Metodo per l'impostazione della skin del blocco.
+     * @param imageName nome dell'immagine che fa da skin.
+     */
     public void setImageFill(String imageName) {
         setImageName(imageName);
         Image pieceImage = new Image(Objects.requireNonNull(getClass().getResource(_image)).toString());
@@ -74,8 +119,43 @@ public class Piece extends Rectangle {
         this.setFill(piecePattern);
     }
 
+
+    //public Color getColor() {return  this._color;}
+
+
+    /**
+     * Metodo per la composizione del path dell'immagine.
+     * @param imageName nome dell'immagine.
+     */
     public void setImageName(String imageName) {
         this._image += imageName;
+    }
+
+
+    //public String getImageName(){ return this._image;}
+
+
+    /**
+     * Metodo che ritorna le dimensioni del blocco in px/100.
+     * @return co array con le dimensioni del blocco [altezza, larghezza].
+     */
+    public int[] getDimensions(){
+        int[] co = new int[2];
+        co[0] = (int)this.getHeight()/100;
+        co[1] = (int)this.getWidth()/100;
+        return co;
+    }
+
+
+    /**
+     * Metodo che ritorna la posizione del blocco in px/100.
+     * @return po array con la posizione del blocco [X, Y].
+     */
+    public int[] getPosition(){
+        int[] po = new int[2];
+        po[0] = (int)this.getLayoutX()/100;
+        po[1] = (int)this.getLayoutY()/100;
+        return po;
     }
 
 
