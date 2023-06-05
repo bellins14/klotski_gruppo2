fa7cb986957c37b543afcab9a80e28a3d8d109ff
lucabord <lucabordin02@gmail.com>
2023-06-05 21:43:30 +0200

    .

--- src/main/java/com/klotski/app/Main.java
+++ src/main/java/com/klotski/app/Main.java
@@ -1,27 +1,27 @@
 package com.klotski.app;
 
-import javafx.application.Application;
+import javafx.application.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //prendo i dati dal file ".fxml"
+        // Carico il file "view.fxml", il quale corrisponde al file che viene utilizzato in Scene Builder per definire
+        //l'interfaccia utente
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        //creo una nuova "scene"
-        Scene scene = new Scene(fxmlLoader.load(),1000,700);
+        //Creo una scena che contiene il contenuto dell'interfaccia utente (1000 x 700)
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setScene(scene);
+        //aggiungo un foglio di stile alla scena corrente
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
-        //Controller controller = fxmlLoader.getController();
-        //imposto una dimensione fissa della schermata, ovvero decido che l'utente non può ingrandirla
+        //impedisco all'utente di ridimensionare la finestra
         stage.setResizable(false);
         stage.show();
-
     }
 
     public static void main(String[] args) {
