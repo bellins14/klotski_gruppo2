19a4ed40a8dfd168cde6d145a30d6b99e30dbf1c
Tommaso Bellinato <tommaso.bellinato@gmail.com>
2023-06-09 01:50:19 +0200

    eliminato elementi non necessari

--- javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"code.eutrustservicesapplication"}];updateSearchResults();
--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"code.eutrustservicesapplication","l":"code.eutrustservicesapplication"}];updateSearchResults();
