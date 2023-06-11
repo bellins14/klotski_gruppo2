dd81e783dd7cabd034f82a75ffbec28d75deb15c
Tommaso Bellinato <tommaso.bellinato@gmail.com>
2023-06-11 16:52:42 +0200

    Javadoc

---
+++ resources/Javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.klotski.app"}];updateSearchResults();
---
+++ resources/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.klotski.app","l":"com.klotski.app"},{"m":"com.klotski.app","l":"com.klotski.jacksonSupport"}];updateSearchResults();
